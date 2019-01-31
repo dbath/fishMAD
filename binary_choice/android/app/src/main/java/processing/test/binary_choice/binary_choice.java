ae4605c9248badd07ced8f30f8618ae085621c73
Dan Bath <danbath@gmail.com>
2019-01-31 14:05:30 +0100

    for alex jordan

--- binary_choice/android/app/src/main/java/processing/test/binary_choice/binary_choice.java
+++ binary_choice/android/app/src/main/java/processing/test/binary_choice/binary_choice.java
@@ -1,4 +1,30 @@
+package processing.test.binary_choice;
 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.io.FileWriter; 
+import java.io.*; 
+import java.util.*; 
+import java.math.BigDecimal; 
+import java.text.DecimalFormat; 
+import java.time.Instant; 
+import java.sql.Timestamp; 
+import java.util.Collections; 
+import controlP5.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class binary_choice extends PApplet {
 
 
 
@@ -6,44 +32,42 @@
 //-------------------------- DO NOT CHANGE VALUES ABOVE------------------------------------
 
 
-float stimDelay = 3.0; //delay between pressing "go" and presentation of stimulus
-float stimDuration = 3.0; //duration of stimulus presentation in seconds 
+float stimDelay = 3.0f; //delay between pressing "go" and presentation of stimulus
+float stimDuration = 3.0f; //duration of stimulus presentation in seconds 
 
-float dotSize = 0.05;  // size of dots in proportion of screen height (eg 0.5 will be half the height of the screen)
-float dotPosX = 0.4; //horizontal distance of dots from centre, in proportion of screen height
-float dotPosY = 0.5; //vertical distance of dots from top, in proportion of screen height
+float dotSize = 0.1f;  // size of dots in proportion of screen height (eg 0.5 will be half the height of the screen)
+float dotPosX = 0.4f; //horizontal distance of dots from centre, in proportion of screen height
+float dotPosY = 0.5f; //vertical distance of dots from top, in proportion of screen height
 
 int defaultBkgColour = 50; //default colour of background from 0 (red) to 100 (red) ROYGBIVR
 int defaultBkgBrightness = 100; //default brightness of background from 0 (black) to 100 (white)
 int defaultBkgSaturation = 0; //default colour of background from 0 (black) to 100 (white)
 
-int defaultdotColour_1 = 33; //default colour of dots from 0 (red) to 100 (red) ROYGBIVR
-int defaultDotBrightness_1 = 100; //default colour of dots from 0 (black) to 100 (bright)
-int defaultSaturation_1 = 100; //default colour of dots from 0 (black) to 100 (white)
+int defaultDotColour_R = 33; //default colour of dots from 0 (red) to 100 (red) ROYGBIVR
+int defaultDotBrightness_R = 100; //default colour of dots from 0 (black) to 100 (bright)
+int defaultSaturation_R = 100; //default colour of dots from 0 (black) to 100 (white)
 
-int defaultdotColour_2 = 0; //default colour of dots from 0 (red) to 100 (red) ROYGBIVR
-int defaultDotBrightness_2 = 100; //default colour of dots from 0 (black) to 100 (white)
-int defaultSaturation_2 = 100; //default colour of dots from 0 (black) to 100 (white)
+int defaultDotColour_L = 0; //default colour of dots from 0 (red) to 100 (red) ROYGBIVR
+int defaultDotBrightness_L = 100; //default colour of dots from 0 (black) to 100 (white)
+int defaultSaturation_L = 100; //default colour of dots from 0 (black) to 100 (white)
 
 
 //-------------------------- DO NOT CHANGE VALUES BELOW------------------------------------
 
 
-import java.io.FileWriter;
-import java.io.*;
-import java.util.*;
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.time.Instant;
-import java.sql.Timestamp;
-import java.util.Collections;
+
+
+
+
+
+
+
+
 
 
 //GUI Setup
 
 
-
-import controlP5.*;
 ControlP5 cP5;
 Slider abc;
 boolean goNoGo = false;
@@ -53,12 +77,12 @@ FileWriter fw;
 BufferedWriter bw;
 DecimalFormat formatter = new DecimalFormat("###.#");
 
-int H;
+
 int t0;
 int tLoop;
 float alpha = 100;
 int hideMode = 0;
-float stopgo = 1.0;
+float stopgo = 1.0f;
 int DIRECTION = 1;
 boolean running = false;
 //Drawing variables
@@ -66,16 +90,16 @@ int sqBar;
 int RANDO = 1;
 
 int bkgColour;
-int dotColour_1;
-int dotColour_2;
+int dotColour_R;
+int dotColour_L;
 int bkgBrightness;
-int dotBrightness_1;
-int dotBrightness_2;
+int dotBrightness_R;
+int dotBrightness_L;
 int bkgSaturation;
-int dotSaturation_1;
-int dotSaturation_2;
+int dotSaturation_R;
+int dotSaturation_L;
 boolean showDots = false;
-boolean NEWFILE = false;
+boolean NEWFILE = true;
 String HEADERS = "TIME" 
                 + '\t' + "BKGCOLOUR" 
                 + '\t' + "BKGBRIGHTNESS"
@@ -93,160 +117,155 @@ String HEADERS = "TIME"
                 + '\t' + "ACTION" 
                 + '\n';
 
-void setup(){
+public void setup(){
 
   
   println(str(height) + '\t' + str(width));
   colorMode(HSB, 100,100,100,100);
-  fullScreen();
+  
   ellipseMode(CENTER);
   
   //adjust sizing relative to screen size:
   
   sqBar = (width - height) /2;
   
-  H = int(0.03*height);
+  int H = PApplet.parseInt(0.03f*height);
   
   // setup GUI
-  
-  
   cP5 = new ControlP5(this);
   
-  
   cP5.addSlider("bkgColour")
-     .setPosition(int(0.1*sqBar),1*H)
+     .setPosition(PApplet.parseInt(0.5f*sqBar),1*H)
      .setSize(200,28)
      .setRange(0,100)
      .setNumberOfTickMarks(51)
      .setValue(defaultBkgColour)
      ;
-    cP5.addSlider("dotColour_1")
-     .setPosition(int(0.1*sqBar),5*H)
+    cP5.addSlider("dotColour_R")
+     .setPosition(PApplet.parseInt(0.5f*sqBar),5*H)
      .setSize(200,28)
      .setRange(0,100)
      .setNumberOfTickMarks(51)
-     .setValue(defaultdotColour_1)
+     .setValue(defaultDotColour_R)
      ;  
-    cP5.addSlider("dotColour_2")
-     .setPosition(int(0.1*sqBar),9*H)
+    cP5.addSlider("dotColour_L")
+     .setPosition(PApplet.parseInt(0.5f*sqBar),9*H)
      .setSize(200,28)
      .setRange(0,100)
      .setNumberOfTickMarks(51)
-     .setValue(defaultdotColour_2)
+     .setValue(defaultDotColour_L)
      ;  
     cP5.addSlider("bkgBrightness")
-     .setPosition(int(0.1*sqBar),3*H)
+     .setPosition(PApplet.parseInt(0.5f*sqBar),3*H)
      .setSize(200,28)
      .setRange(0,100)
      .setNumberOfTickMarks(51)
      .setValue(defaultBkgBrightness)
      ;
-    cP5.addSlider("dotBrightness_1")
-     .setPosition(int(0.1*sqBar),7*H)
+    cP5.addSlider("dotBrightness_R")
+     .setPosition(PApplet.parseInt(0.5f*sqBar),7*H)
      .setSize(200,28)
      .setRange(0,100)
      .setNumberOfTickMarks(51)
-     .setValue(defaultDotBrightness_1)
+     .setValue(defaultDotBrightness_R)
      ;  
-    cP5.addSlider("dotBrightness_2")
-     .setPosition(int(0.1*sqBar),11*H)
+    cP5.addSlider("dotBrightness_L")
+     .setPosition(PApplet.parseInt(0.5f*sqBar),11*H)
      .setSize(200,28)
      .setRange(0,100)
      .setNumberOfTickMarks(51)
-     .setValue(defaultDotBrightness_2)
+     .setValue(defaultDotBrightness_L)
      ;  
     cP5.addSlider("bkgSaturation")
-     .setPosition(int(0.1*sqBar),2*H)
+     .setPosition(PApplet.parseInt(0.5f*sqBar),2*H)
      .setSize(200,28)
      .setRange(0,100)
      .setNumberOfTickMarks(51)
      .setValue(defaultBkgSaturation)
      ;
-    cP5.addSlider("dotSaturation_1")
-     .setPosition(int(0.1*sqBar),6*H)
+    cP5.addSlider("dotSaturation_R")
+     .setPosition(PApplet.parseInt(0.5f*sqBar),6*H)
      .setSize(200,28)
      .setRange(0,100)
      .setNumberOfTickMarks(51)
-     .setValue(defaultSaturation_1)
+     .setValue(defaultSaturation_R)
      ;  
-    cP5.addSlider("dotSaturation_2")
-     .setPosition(int(0.1*sqBar),10*H)
+    cP5.addSlider("dotSaturation_L")
+     .setPosition(PApplet.parseInt(0.5f*sqBar),10*H)
      .setSize(200,28)
      .setRange(0,100)
      .setNumberOfTickMarks(51)
-     .setValue(defaultSaturation_2)
+     .setValue(defaultSaturation_L)
      ;  
     cP5.addSlider("dotSize")
-     .setPosition(int(0.1*sqBar),13*H)
+     .setPosition(PApplet.parseInt(0.5f*sqBar),13*H)
      .setSize(200,28)
-     .setRange(0,0.1)
+     .setRange(0,0.5f)
      .setNumberOfTickMarks(51)
      .setValue(dotSize)
      ;  
     cP5.addSlider("dotPosX")
-     .setPosition(int(0.1*sqBar),14*H)
+     .setPosition(PApplet.parseInt(0.5f*sqBar),14*H)
      .setSize(200,28)
      .setRange(0,1)
      .setNumberOfTickMarks(51)
      .setValue(dotPosX)
      ;  
     cP5.addSlider("dotPosY")
-     .setPosition(int(0.1*sqBar),15*H)
+     .setPosition(PApplet.parseInt(0.5f*sqBar),15*H)
      .setSize(200,28)
      .setRange(0,1)
      .setNumberOfTickMarks(51)
      .setValue(dotPosY)
      ;  
     cP5.addSlider("stimDelay")
-     .setPosition(int(0.1*sqBar),17*H)
+     .setPosition(PApplet.parseInt(0.5f*sqBar),17*H)
      .setSize(200,28)
      .setRange(0,60)
      .setNumberOfTickMarks(61)
      .setValue(3)
      ;  
     cP5.addSlider("stimDuration")
-     .setPosition(int(0.1*sqBar),18*H)
+     .setPosition(PApplet.parseInt(0.5f*sqBar),18*H)
      .setSize(200,28)
      .setRange(0,60)
      .setNumberOfTickMarks(61)
      .setValue(3)
      ;  
-    cP5.addBang("Report_Settings",int(0.1*sqBar),20*H,60,30);
-    //cP5.addBang("Hide",sqBar-220,300,60,30);
-    //cP5.addBang("Reverse",sqBar-220,350,60,30);
-    cP5.addBang("Go",int(0.5*sqBar),20*H,60,30);
-    cP5.addToggle("SETUP")
-     .setPosition(int(0.5*sqBar),22*H)
-     .setSize(60,30);
+  cP5.addBang("Report_Settings",PApplet.parseInt(0.5f*sqBar),20*H,60,30);
+  //cP5.addBang("Hide",sqBar-220,300,60,30);
+  //cP5.addBang("Reverse",sqBar-220,350,60,30);
+  cP5.addBang("Go",PApplet.parseInt(0.8f*sqBar),20*H,60,30);
+  cP5.addBang("SETUP",PApplet.parseInt(0.8f*sqBar),22*H,60,30);
    
   stroke(0,0,0,0);
 
-  logEntry("PROGRAM INITIATED", true);
+  String TIME = RTFN();
+  logEntry(TIME + '\t' + "PROGRAM INITIATED", true);
 }
 
-void keyPressed(){
+public void keyPressed(){
 
   if (key == 'r'){Report_Settings();}
   else if (key == 'g'){Go();                }
-  else if (key == 's'){SETUP();                }
   else {logEntry( "KEYSTROKE:" + str(key), true);}
   
 }
 
-void SETUP(){
+public void SETUP(){
   SETUP = !SETUP;
 }
 
 
-void Report_Settings(){
+public void Report_Settings(){
     logEntry("REPORT SETTINGS", true);
 }
 
 
 
-void Go(){
+public void Go(){
   SETUP = false;
-  RANDO = int(pow(-1, int(random(0,100))));
+  RANDO = PApplet.parseInt(pow(-1, PApplet.parseInt(random(0,100))));
   t0 = millis();
   goNoGo = true;
   logEntry("START", true);
@@ -254,13 +273,12 @@ void Go(){
 
 
 
-void logEntry(String msg, boolean append) { 
+public void logEntry(String msg, boolean append) { 
   
   try {
     File file =new File("/sdcard/dotbot/log_binaryChoice.txt");
     if (!file.exists()) {
       file.createNewFile();
-      NEWFILE = true;
     }
  
     FileWriter fw = new FileWriter(file, append);///true = append
@@ -269,19 +287,18 @@ void logEntry(String msg, boolean append) {
     if (NEWFILE == true){
       pw.write(HEADERS);
       println(HEADERS);
-      NEWFILE = false;
     }
 
     String defaultInfo = RTFN() 
                 + '\t' + str(bkgColour)
                 + '\t' + str(bkgBrightness)
                 + '\t' + str(bkgSaturation)
-                + '\t' + str(dotColour_1)
-                + '\t' + str(dotBrightness_1)
-                + '\t' + str(dotSaturation_1)
-                + '\t' + str(dotColour_2)
-                + '\t' + str(dotBrightness_2)
-                + '\t' + str(dotSaturation_2)
+                + '\t' + str(dotColour_R)
+                + '\t' + str(dotBrightness_R)
+                + '\t' + str(dotSaturation_R)
+                + '\t' + str(dotColour_L)
+                + '\t' + str(dotBrightness_L)
+                + '\t' + str(dotSaturation_L)
                 + '\t' + str(dotSize)
                 + '\t' + str(dotPosX)
                 + '\t' + str(dotPosY)
@@ -304,24 +321,24 @@ public String RTFN(){
 }
 
 
-void squareFrame(){
+public void squareFrame(){
   fill(0,0,0,100);
   rect(0,0,sqBar, height);
   rect(width - sqBar,0,sqBar, height);
 }
 
 
-void draw(){
+public void draw(){
   background(bkgColour,bkgSaturation,bkgBrightness,100);
 
-  int dotSize_adj = int(dotSize*height);
-  int dotPosX_adj = int(dotPosX*height);
-  int dotPosY_adj = int(dotPosY*height);
+  int dotSize_adj = PApplet.parseInt(dotSize*height);
+  int dotPosX_adj = PApplet.parseInt(dotPosX*height);
+  int dotPosY_adj = PApplet.parseInt(dotPosY*height);
   if (SETUP == true){
-    fill(dotColour_2, dotSaturation_2, dotBrightness_2, 100);
-    ellipse(width/2 - dotPosX_adj, dotPosY_adj, 1.4*dotSize_adj, dotSize_adj);
-    fill(dotColour_1, dotSaturation_1, dotBrightness_1, 100);
-    ellipse(width/2 + dotPosX_adj, dotPosY_adj, 1.4*dotSize_adj, dotSize_adj);
+    fill(dotColour_L, dotSaturation_L, dotBrightness_L, 100);
+    ellipse(width/2 - dotPosX_adj, dotPosY_adj, 1.4f*dotSize_adj, dotSize_adj);
+    fill(dotColour_R, dotSaturation_R, dotBrightness_R, 100);
+    ellipse(width/2 + dotPosX_adj, dotPosY_adj, 1.4f*dotSize_adj, dotSize_adj);
   }
     
   else if (goNoGo == true){
@@ -336,17 +353,15 @@ void draw(){
         logEntry("STIM ON", true);
         showDots = true;
       }
-      fill(dotColour_2, dotSaturation_2, dotBrightness_2, 100);
-      ellipse(width/2 - RANDO*dotPosX_adj, dotPosY_adj, 1.4*dotSize_adj, dotSize_adj);
-      fill(dotColour_1, dotSaturation_1, dotBrightness_1, 100);
-      ellipse(width/2 + RANDO*dotPosX_adj, dotPosY_adj, 1.4*dotSize_adj, dotSize_adj);
+      fill(dotColour_L, dotSaturation_L, dotBrightness_L, 100);
+      ellipse(width/2 - RANDO*dotPosX_adj, dotPosY_adj, 1.4f*dotSize_adj, dotSize_adj);
+      fill(dotColour_R, dotSaturation_R, dotBrightness_R, 100);
+      ellipse(width/2 + RANDO*dotPosX_adj, dotPosY_adj, 1.4f*dotSize_adj, dotSize_adj);
     }
     tLoop = millis();
 
   }
   squareFrame();
-  fill(0,0,100,100);
-  textSize(40);
-  if (RANDO == 1){  text("Cue 1 is right", int(0.2*sqBar),28*H);}
-  else if (RANDO == -1){  text("Cue 1 is left", int(0.2*sqBar),28*H);}
+}
+  public void settings() {  fullScreen(); }
 }
